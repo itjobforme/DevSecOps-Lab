aecd1f8ea852dfe70ee932a16d9742974a69480c
Jonathan Davis <111515260+itjobforme@users.noreply.github.com>
2025-02-18 13:32:47 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,7 +138,7 @@ resource "aws_internet_gateway" "devsecops_igw" {
 ### Create Public Subnet 1
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.devsecops_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
