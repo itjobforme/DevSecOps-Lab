afd3a1b9f595176fdcc87d44b4a9af6e2f5aab98
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-20 22:07:26 -0600

    Create models.py

---
+++ blog-app/models.py
@@ -0,0 +1,22 @@
+from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# User Model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    otp_secret = db.Column(db.String(16), nullable=True, default=None)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Blog Post Model
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
