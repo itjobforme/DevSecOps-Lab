2a4f78d63b9963ba22ff59c327d60349aa946a6f
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-20 21:57:01 -0600

    Update admin.py

--- blog-app/admin.py
+++ blog-app/admin.py
@@ -1,43 +1,20 @@
-from flask_admin import Admin, AdminIndexView, expose
+from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_login import current_user, login_required
+from flask_login import current_user
 from flask import redirect, url_for
-
 from app import db, User, BlogPost
 
-# Secure Admin Panel for Users
-class SecureUserModelView(ModelView):
-    form_columns = ['username', 'password']
-    form_extra_fields = {
-        'password': {'type': 'PasswordField', 'label': 'New Password'}
-    }
+admin = Admin(name="Blog Admin", template_mode="bootstrap3")
 
-    def on_model_change(self, form, model, is_created):
-        if form.password.data:
-            model.set_password(form.password.data)
+class SecureModelView(ModelView):
+    column_exclude_list = ['password_hash', 'otp_secret']
+    form_excluded_columns = ['password_hash', 'otp_secret']
 
     def is_accessible(self):
         return current_user.is_authenticated
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
-# Secure Admin Panel for Blog Posts
-class SecureBlogPostModelView(ModelView):
-    form_columns = ['title', 'content']
-
-    def is_accessible(self):
-        return current_user.is_authenticated
-
-    def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login'))
-
-class SecureAdminIndexView(AdminIndexView):
-    @expose('/')
-    @login_required
-    def index(self):
-        return super().index()
-
-admin = Admin(name='Blog Admin', template_mode='bootstrap3', index_view=SecureAdminIndexView())
-admin.add_view(SecureUserModelView(User, db.session))
-admin.add_view(SecureBlogPostModelView(BlogPost, db.session))
+admin.add_view(SecureModelView(User, db.session))
+admin.add_view(SecureModelView(BlogPost, db.session))
