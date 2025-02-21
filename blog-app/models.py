57f6afffe80aabdef213d0daf0be1d4d43e93e84
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-20 22:13:29 -0600

    Update models.py

--- blog-app/models.py
+++ blog-app/models.py
@@ -1,6 +1,8 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
 
 # User Model
 class User(db.Model, UserMixin):
