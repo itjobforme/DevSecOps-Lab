b506cac436a4e7b4a63caea2985bbf7e295ab67a
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 14:25:48 -0600

    Update backend.tf

--- terraform/azure/backend.tf
+++ terraform/azure/backend.tf
@@ -1,7 +1,9 @@
-provider "azurerm" {
-  features {}
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "devsecops-lab-rg"     
+    storage_account_name  = "devsecopsterraform"   
+    container_name        = "tfstate"             
+    key                   = "terraform.tfstate"     
+  }
 }
+
