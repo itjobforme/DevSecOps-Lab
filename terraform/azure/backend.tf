dc633c2b7d1fc508cd7b4a37afe3a071f0ed123f
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 14:37:29 -0600

    Update backend.tf

--- terraform/azure/backend.tf
+++ terraform/azure/backend.tf
@@ -3,7 +3,12 @@ terraform {
     resource_group_name   = "devsecops-lab-rg"     
     storage_account_name  = "devsecopsterraform"   
     container_name        = "tfstate"             
-    key                   = "terraform.tfstate"     
+    key                   = "terraform.tfstate"
+
+    # Service Principal Authentication for Backend
+    tenant_id       = var.tenant_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    subscription_id = var.subscription_id
   }
 }
-
