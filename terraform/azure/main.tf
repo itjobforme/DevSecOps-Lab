c19ccf440f9d1f5389107dd8da0feb8e5cb2aa29
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 15:10:35 -0600

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,11 +3,30 @@ provider "azurerm" {
   use_oidc = true
 }
 
+# Resource Group for the AKS Cluster
 resource "azurerm_resource_group" "devsecops_rg" {
   name     = "devsecops-lab-rg"
   location = "East US"
 }
 
-module "storage" {
-  source = "./storage.tf"
+# Create a Storage Account for Terraform State
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "devsecopsterraform"
+  resource_group_name      = azurerm_resource_group.devsecops_rg.name
+  location                 = azurerm_resource_group.devsecops_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_blob_public_access = false
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Create a Container for Terraform State Files
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
 }
