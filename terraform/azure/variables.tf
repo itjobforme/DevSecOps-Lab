957aed6af65d9cc7514b9dde0e9f692c329648e4
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 14:49:18 -0600

    Update variables.tf

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,16 +1,1 @@
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-}
 
-variable "client_id" {
-  description = "Azure Client ID"
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-}
