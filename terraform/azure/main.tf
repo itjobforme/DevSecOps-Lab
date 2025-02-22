ba17e8369e8622c142932103a0e95c54eb8e0a42
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 15:19:12 -0600

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,7 +3,6 @@ provider "azurerm" {
   use_oidc = true
 }
 
-# Resource Group for the AKS Cluster
 resource "azurerm_resource_group" "devsecops_rg" {
   name     = "devsecops-lab-rg"
   location = "East US"
