2d286092aa063b7043d9118ff19600e0b7b169b1
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 15:30:36 -0600

    Update ingress.tf

--- terraform/azure/ingress.tf
+++ terraform/azure/ingress.tf
@@ -1,11 +1,11 @@
 resource "azurerm_public_ip" "ingress_ip" {
   name                = "devsecops-ingress-ip"
-  resource_group_name = azurerm_resource_group.devsecops_rg.name
-  location            = azurerm_resource_group.devsecops_rg.location
+  resource_group_name = data.azurerm_resource_group.devsecops_rg.name
+  location            = data.azurerm_resource_group.devsecops_rg.location
   allocation_method   = "Static"
   sku                 = "Standard"
 }
-#
+
 output "ingress_ip" {
   value = azurerm_public_ip.ingress_ip.ip_address
 }
