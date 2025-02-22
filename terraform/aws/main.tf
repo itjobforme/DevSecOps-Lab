3bca2d71f21aa2f0f5fbbe62adc80daade68f30d
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 13:31:13 -0600

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -300,7 +300,6 @@ resource "aws_security_group" "devsecops_sg" {
   }
 }
 
-# EC2 Instance Configuration
 resource "aws_instance" "devsecops_blog" {
   ami                         = "ami-09e67e426f25ce0d7"
   instance_type               = "t2.micro"
