dbdf553608fdd2aef4af354509be8b9db31f08b9
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 15:06:08 -0600

    Update backend.tf

--- terraform/azure/backend.tf
+++ terraform/azure/backend.tf
@@ -3,7 +3,6 @@ terraform {
     resource_group_name   = "devsecops-lab-rg"     
     storage_account_name  = "devsecopsterraform"   
     container_name        = "tfstate"             
-    key                   = "terraform.tfstate"
-
+    key                   = "terraform.tfstate"     
   }
 }
