b73a01a34733a54d6ef3d1436f8ee9c967b814e3
itjobforme <itjobforme@hotmail.com>
2025-02-22 13:24:46 -0600

    Add Azure Terraform configuration with persistent state in Azure Storage

---
+++ terraform/azure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "devsecops-lab-rg"
+    storage_account_name  = "devsecopsterraform"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -7,6 +7,33 @@ resource "azurerm_resource_group" "devsecops_rg" {
   location = "East US"
 }
 
+# Azure Resource Group
+resource "azurerm_resource_group" "devsecops_rg" {
+  name     = "devsecops-lab-rg"
+  location = "East US"
+}
+
+# Storage Account for Terraform State
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "devsecopsterraform"
+  resource_group_name      = azurerm_resource_group.devsecops_rg.name
+  location                 = azurerm_resource_group.devsecops_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "devsecops"
+  }
+}
+
+# Blob Container for State Files
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+
 module "aks" {
   source = "./aks"
   resource_group_name = azurerm_resource_group.devsecops_rg.name
