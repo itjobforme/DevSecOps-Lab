bd06fb9fa81260d185fd645ea67acb400e51ed53
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 14:42:30 -0600

    Update backend.tf

--- terraform/azure/backend.tf
+++ terraform/azure/backend.tf
@@ -5,10 +5,5 @@ terraform {
     container_name        = "tfstate"             
     key                   = "terraform.tfstate"
 
-    # Service Principal Authentication for Backend
-    tenant_id       = var.tenant_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    subscription_id = var.subscription_id
   }
 }
