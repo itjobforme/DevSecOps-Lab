a3fea17dbb917c1bd17081be4275d2cddee0b698
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 15:08:14 -0600

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,11 +1,13 @@
 provider "azurerm" {
   features {}
-
-  # Use the default Azure CLI authentication (via OIDC)
   use_oidc = true
 }
-#
+
 resource "azurerm_resource_group" "devsecops_rg" {
   name     = "devsecops-lab-rg"
   location = "East US"
+}
+
+module "storage" {
+  source = "./storage.tf"
 }
