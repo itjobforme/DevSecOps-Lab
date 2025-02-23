87fe05e157ccdef1a429506d36954bb3d1eb2fc0
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-23 00:14:29 -0600

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -90,9 +90,9 @@ resource "aws_eks_cluster" "devsecops_eks" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids       = aws_subnet.eks_subnets[*].id
+    subnet_ids         = aws_subnet.eks_subnets[*].id
     security_group_ids = [aws_security_group.eks_node_sg.id]
-    version: = "1.32"
+    version:           = "1.32"
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
