c2f13a105d539cdb42c97fa36afb06f156110262
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:51:20 -0600

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -4,7 +4,7 @@ module "eks_vpc" {
 
   name = "devsecops-eks-vpc"
   cidr = var.vpc_cidr
-
+#
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
