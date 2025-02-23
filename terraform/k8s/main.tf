31f060935c2329e6fd74cf6a3cb3816cc2c5ea8e
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-23 10:33:46 -0600

    Update main.tf

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -11,9 +11,9 @@ terraform {
 }
 
 resource "aws_instance" "k8s_app_ec2" {
-  ami           = "ami-08962a4068733a2b6" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  ami           = "ami-09e67e426f25ce0d7" # Ubuntu Server 20.04 LTS
   instance_type = "t2.micro"
-  key_name      = "devsecops-key" # Updated to use devsecops-key.pem
+  iam_instance_profile = "SSMManagedInstanceProfile" # Use SSM for management
 
   security_groups = ["k8s-app-sg"]
 
@@ -24,6 +24,7 @@ resource "aws_instance" "k8s_app_ec2" {
     sudo systemctl start docker
     sudo usermod -aG docker ubuntu
 
+    # Install aws cli
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
@@ -86,13 +87,6 @@ resource "aws_instance" "k8s_app_ec2" {
 
 resource "aws_security_group" "k8s_app_sg" {
   name        = "k8s-app-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["173.216.28.115/32"]
-  }
 
   ingress {
     from_port   = 80
