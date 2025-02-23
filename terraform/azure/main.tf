4492b9938ebc957748504f11a98601af5e0aa3e5
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:23:42 -0600

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -6,3 +6,11 @@ provider "azurerm" {
 data "azurerm_resource_group" "devsecops_rg" {
   name = "devsecops-lab-rg"
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
+
