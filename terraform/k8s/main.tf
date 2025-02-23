5650469ee09bf6ab31da60f3c22ab21e1f2eb1d2
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-23 15:05:21 -0600

    Update main.tf

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -213,6 +213,13 @@ resource "aws_security_group" "k8s_app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
