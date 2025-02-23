2c8f6a689182804d921b39df824624b552b1db39
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:16:44 -0600

    Update variables.tf

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,1 +1,10 @@
+variable "tls_certificate" {
+  description = "Base64-encoded TLS certificate"
+  sensitive   = true
+}
+
+variable "tls_private_key" {
+  description = "Base64-encoded TLS private key"
+  sensitive   = true
+}
 
