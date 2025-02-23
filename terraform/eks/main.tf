ce646f2ac3151e3e3fa23c4f5110bbd45b979660
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:47:34 -0600

    Update and rename mian.tf to main.tf

---
+++ terraform/eks/main.tf
@@ -0,0 +1,19 @@
+module "eks_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "devsecops-eks-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "devsecops-eks-vpc"
+  }
+}
+
