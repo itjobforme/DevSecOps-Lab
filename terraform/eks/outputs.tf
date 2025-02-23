7dfda39ff08868a02f560b458d5c69ab2a4eb220
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:49:10 -0600

    Update outputs.tf

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,1 +1,12 @@
+output "eks_cluster_name" {
+  value = module.eks_cluster.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks_cluster.cluster_endpoint
+}
+
+output "eks_cluster_security_group_id" {
+  value = module.eks_cluster.cluster_security_group_id
+}
 
