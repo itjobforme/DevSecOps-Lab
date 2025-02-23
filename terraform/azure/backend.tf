e9bfafa917559d14220a581dfab598b14042c121
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:35:46 -0600

    Delete terraform/azure directory

--- terraform/azure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "devsecops-lab-rg"
-    storage_account_name  = "devsecopsterraform1"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
