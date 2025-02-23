7278cfa80dbcab99d71764ab062a75c0e1f52d7c
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 18:14:50 -0600

    Update ingress.tf

--- terraform/azure/ingress.tf
+++ terraform/azure/ingress.tf
@@ -1,11 +1,24 @@
-resource "azurerm_public_ip" "ingress_ip" {
-  name                = "devsecops-ingress-ip"
-  resource_group_name = data.azurerm_resource_group.devsecops_rg.name
-  location            = data.azurerm_resource_group.devsecops_rg.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-output "ingress_ip" {
-  value = azurerm_public_ip.ingress_ip.ip_address
-}
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: devsecops-k8s-ingress
+  annotations:
+    kubernetes.io/ingress.class: "nginx"
+    nginx.ingress.kubernetes.io/rewrite-target: /
+    nginx.ingress.kubernetes.io/ssl-redirect: "true"
+spec:
+  tls:
+    - hosts:
+        - k8s.securingthecloud.org
+      secretName: tls-secret
+  rules:
+    - host: k8s.securingthecloud.org
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: devsecops-k8s-service
+                port:
+                  number: 443
