18661aeb1100cf800e61517a7545d9042a46b365
jonathan-davis-soteria <164948908+jonathan-davis-soteria@users.noreply.github.com>
2025-02-22 23:17:44 -0600

    Create alb-security-group.tf

---
+++ terraform/eks/alb-security-group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "devsecops-alb-sg"
+  vpc_id = aws_vpc.devsecops_eks_vpc.id
+  description = "Allow inbound traffic to ALB"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic from anywhere"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS traffic from anywhere"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "devsecops-alb-sg"
+  }
+}
